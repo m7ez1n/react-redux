80b57fe8962d8522a57a2b546d1b0c50eb13782f
m7Aei <mateuspm2015@hotmail.com>
2019-10-04 13:28:58 -0300

    Fazendo a estilização e a home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,191 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-masculino/08/COL-4399-108/COL-4399-108_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/10/E74-0492-010/E74-0492-010_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/36/COL-3586-036/COL-3586-036_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-zeus-esportivo-masculino/18/337-9918-118/337-9918-118_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-masculino/14/COL-6981-014/COL-6981-014_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-esporte-adaption-masculino/14/KTM-0031-014/KTM-0031-014_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-grand-court-base-masculino/06/COL-7146-006/COL-7146-006_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-8k-masculino/14/COL-4865-014/COL-4865-014_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis adidas</strong>
+        <span>R$299,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={14} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
