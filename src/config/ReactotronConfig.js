7824c14d91be3958aa8a6a5fd9d0df2fdb105d9f
m7Aei <mateuspm2015@hotmail.com>
2019-10-07 15:28:42 -0300

    Configurando o plugin do reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reactrotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
     .use(reactotronRedux())
+    .use(reactrotronSaga())
     .connect();
 
   tron.clear();
