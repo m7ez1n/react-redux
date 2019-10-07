2fddc6c5db6c62eeaaa96936cb25b94105175443
m7Aei <mateuspm2015@hotmail.com>
2019-10-07 15:09:21 -0300

    Refatorando a action de adicionar ao carrinho com redux-saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,10 +26,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -45,7 +45,7 @@ class Home extends Component {
             <span>{product.priceFormatted}</span>
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdShoppingCart size={14} color="#FFF" />{' '}
